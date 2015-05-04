88beedd179913ab73988e58d502e44da99ccf6d9
nEo <agat@isightpartners.com>
2015-05-04 14:56:49 +0530

    Typing Errors:2

--- pcap_public.py
+++ pcap_public.py
@@ -68,13 +68,13 @@ for timestamp, buf in pcap.readpkts():
 						    if str1<> "" : dns_list.append(str1)
 
 	    if tcp.__class__.__name__ == 'TCP':
-            if tcp.dport in http_ports and len(tcp.data) > 0:
-                try:
-                    http = dpkt.http.Request(tcp.data)
-                    urls.append(http.headers['host'] + http.uri)
-                except Exception as e:
-                    # Just in case we come across some stubborn kid.
-                    print "[-] Some error occured. - %s" % str(e)
+                if tcp.dport in http_ports and len(tcp.data) > 0:
+                    try:
+                        http = dpkt.http.Request(tcp.data)
+                        urls.append(http.headers['host'] + http.uri)
+                    except Exception as e:
+                        # Just in case we come across some stubborn kid.
+                        print "[-] Some error occured. - %s" % str(e)
 f.close()
 print "[+] URLs extracted from PCAP file are:"
 for url in urls:
